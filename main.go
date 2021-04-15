7a1a3492bd3e23b85ed6396478b8999cbe6b8e51
alyaskastorm <minealaska29@gmail.com>
2021-04-16 00:02:04 +0300

    try range in templates

--- main.go
+++ main.go
@@ -8,19 +8,36 @@ import (
 	"github.com/gofiber/template/html"
 )
 
+type Item struct {
+	Str1, Str2 string
+}
+
+type ViewData struct {
+	Items []Item
+}
+
 func main() {
 
 	engine := html.New("./views", ".html")
+
 	app := fiber.New(fiber.Config{
 		Views: engine,
 	})
 
 	app.Get("/", func(c *fiber.Ctx) error {
-
+		d := new(ViewData)
+		d.Items = []Item{
+			Item{
+				Str1: getRandomString(2),
+				Str2: getRandomString(4)},
+			Item{
+				Str1: getRandomString(2),
+				Str2: getRandomString(4),
+			}}
 		return c.Render("index", fiber.Map{
 			"Title": getRandomString(10),
 			"Some":  getRandomString(15),
-		})
+			"Items": d.Items})
 	})
 
 	log.Fatal(app.Listen(":3000"))
