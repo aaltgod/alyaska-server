f843122b4651f976f28cee270789e8837d0bca3d
alyaskastorm <minealaska29@gmail.com>
2021-05-23 15:54:18 +0300

    Add icons

--- frontend/src/components/Upload.vue
+++ frontend/src/components/Upload.vue
@@ -7,17 +7,21 @@
                 </label>
             </div>
             <div class="large-12 medium-12 small-12 cell">
-                <div v-for="(file, key) in files" class="file-listing" v-bind:key="file">{{ file.name }} <span class="remove-file" v-on:click="removeFile( key )">Remove</span></div>
+                <div v-for="(file, key) in files" class="file-listing" v-bind:key="file">
+                    {{ file.name }} 
+                    <i class="fa fa-window-close" style="cursor: pointer" aria-hidden="true" v-on:click="removeFile( key )"></i>
+                </div>
             </div>
             <br>
-            <div class="large-12 medium-12 small-12 cell">
-                <button v-on:click="addFile()">Add Files</button>
-            </div>
             
             <br>
             <div class="large-12 medium-12 small-12 cell">
                 <button v-on:click="submitFile()">Submit</button>
             </div>
+            <h3 v-if="loading">
+                <i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
+                <span class="sr-only">Loading...</span>
+            </h3>
             <h3><a v-bind:href="'/folder/'+folderName">{{ folderName }}</a></h3>
             <h3><a v-if="filesSizeError">Files size > 250Mb</a></h3> 
         </div>
@@ -33,14 +37,11 @@ import axios from 'axios'
                 files:[],
                 folderName: "",
                 filesSizeError: false,
+                loading: false,
             }
         },
 
         methods: {
-            addFile(){
-                this.$refs.files.click()
-            },
-
             submitFile(){
                 let formData = new FormData()
 
@@ -54,6 +55,7 @@ import axios from 'axios'
                     let file = this.files[i]
                     
                     filesSize += file.size
+
                     formData.append(i, file)
                 }
 
@@ -64,6 +66,8 @@ import axios from 'axios'
                     return
                 }
 
+                this.loading = true
+
                 axios.post(process.env.VUE_APP_API_URL+"/api/upload-file",
                 formData,
                 {
@@ -72,18 +76,21 @@ import axios from 'axios'
                     }
                 }).then(result =>{
                     this.folderName = result.data["folderName"]
+
+                    this.loading = false
                 }).catch(e =>{
                     console.error(e)
+
+                    this.loading = false
                 })
-                
             },
 
             handleFileUpload(){
                 let uploadedFiles = this.$refs.files.files
-                
-                for( var i=0; i < uploadedFiles.length; i++ ){
+                for( var i = 0; i < uploadedFiles.length; i++ ){
                     this.files.push( uploadedFiles[i] )
                 }
+                
             },
 
             removeFile( key ){
